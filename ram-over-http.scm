e847be898c4f944feb35c8b6d2bb6268625622c1
ktakashi <ktakashi@ymail.com>
2013-09-20 12:31:58 +0200

    fixed typo

--- ram-over-http.scm
+++ ram-over-http.scm
@@ -27,7 +27,7 @@
 ;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ;;;
-(library (ram-oever-http)
+(library (ram-over-http)
     (export start-session)
     (import (rnrs)
 	    (sagittarius)
